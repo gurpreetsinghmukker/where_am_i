ede3aa0ae77432840aec0dd793d5b5d11b2e5a4f
Gurpreet <gurpreetsinghmukker@gmail.com>
2022-07-21 11:09:02 -0400

    testing git ignore

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,6 +7,7 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH='/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu'
-export PKG_CONFIG_PATH='/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig'
+export CMAKE_PREFIX_PATH="/home/gurpreet/workspace/project_3/catkin_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD='/home/gurpreet/workspace/project_3/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/gurpreet/workspace/project_3/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/gurpreet/workspace/project_3/catkin_ws/src:$ROS_PACKAGE_PATH"
